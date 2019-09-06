7add7e5ad1dde07a2b3775354e61bf4c095ee73c
尹旭 <yin_8023@addcn.com>
2019-09-06 18:34:33 +0800

    新增未实现的交错页面

---
+++ flutter_animation/lib/com.demo.animated/other/StaggeredAnimationPage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+///交错动画
+class StaggeredAnimationPage extends StatefulWidget {
+  @override
+  StaggeredAnimationPageState createState() => new StaggeredAnimationPageState();
+}
+
+class StaggeredAnimationPageState extends State<StaggeredAnimationPage> {
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(''),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
